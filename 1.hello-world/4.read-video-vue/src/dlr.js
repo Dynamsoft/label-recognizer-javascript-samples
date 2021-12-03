f7d1c5efefe4ce956d820206468d823b1164f842
RIOTSAKURA <1192587294@qq.com>
2021-12-03 08:49:20 +0800

    remove vue sample

--- 1.hello-world/4.read-video-vue/src/dlr.js
+++
@@ -1,6 +0,0 @@
-import { LabelRecognizer } from "keillion-dynamsoft-label-recognizer";
-
-LabelRecognizer.engineResourcePath = "https://cdn.jsdelivr.net/npm/keillion-dynamsoft-label-recognizer@0.20211123171208.0/dist/";
-LabelRecognizer.initLicense("t0068MgAAAKUZULwM1SshTkeSYEV5LPo0cnYXkSYBlpGkb0XcP1NTLvP+//NMWCbQfuyxGn1hbLfqF/bV7FuoMzNPJljQqW0=");
-
-export default LabelRecognizer;

69a7dc751d6650d234f8c5c125e513bfda66a8c2
Cube <877211593@qq.com>
2022-04-14 11:22:57 +0800

    update license

--- 1.hello-world/7.read-video-nextjs/dlr.js
+++ 1.hello-world/7.read-video-nextjs/dlr.js
@@ -5,7 +5,7 @@ LabelRecognizer.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-lab
  * To use the library, you need to first specify a license key using the API "license" as shown below.
  */
 
-LabelRecognizer.license = "DLS2eyJtYWluU2VydmVyVVJMIjoiaHR0cHM6Ly90ZXN0ZGxzLmR5bmFtc29mdC5jb20iLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
+LabelRecognizer.license = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
 /** 
  * You can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=github&product=dlr&package=js to get your own trial license good for 30 days. 
  * Note that if you downloaded this sample from Dynamsoft while logged in, the above license key may already be your own 30-day trial license.

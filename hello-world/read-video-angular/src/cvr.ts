91f571931cff7dc4f0b5bff1da77fde83bdbf5bb
Tom Kent <Tom-Dynamsoft@users.noreply.github.com>
2024-07-30 00:31:08 -0700

    Bug: add version link to tracked link

--- hello-world/read-video-angular/src/cvr.ts
+++ hello-world/read-video-angular/src/cvr.ts
@@ -14,7 +14,7 @@ LicenseManager.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9");
 /**
  * You can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=github&product=dlr&package=js to get your own trial license good for 30 days.
  * Note that if you downloaded this sample from Dynamsoft while logged in, the above license key may already be your own 30-day trial license.
- * For more information, see https://www.dynamsoft.com/label-recognition/docs/web/programming/javascript/user-guide/index.html#specify-the-license or contact support@dynamsoft.com.
+ * For more information, see https://www.dynamsoft.com/label-recognition/docs/web/programming/javascript/user-guide/index.html?ver=3.2.30#specify-the-license or contact support@dynamsoft.com.
  * LICENSE ALERT - THE END
  */
 
